d3a9dee91f6f770564e6dfe883e6e6f16867f370
jcpoulin <jcpoulin@ualberta.ca>
2020-04-02 14:37:32 -0600

    Starting markdown

--- sd/views.py
+++ sd/views.py
@@ -85,6 +85,7 @@ def feed(request):
                 # temp = Author.objects.get(fs)
                 
             all_posts = all_posts.distinct()
+            makeCommonMark(all_posts)
             results = paginated_result(request, all_posts, GetPostSerializer, "feed", query="feed")
             return render(request, 'sd/main.html', {'current_user': user, 'authenticated': True, 'results': results})
         else:
@@ -93,6 +94,10 @@ def feed(request):
     else:
         return HttpResponse(status_code=405)
 
+
+def makeCommonMark(posts):
+    for p in posts:
+        print(p)
 
 def account(request):
     if valid_method(request):
