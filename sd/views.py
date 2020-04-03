1a2abfffd88a5eaddcd755151fe153341e7bfbec
wlt30 <wlt@ualberta.ca>
2020-04-03 17:29:10 -0600

    testing on comments

--- sd/views.py
+++ sd/views.py
@@ -40,7 +40,7 @@ def explore(request):
             comments = []
             for c in all_comments:
                 comments.append({
-                    'post':c.post.uuid,
+                    'post':c.post,
                     'author': c.author,
                     'comment': c.comment,
                     'published': c.published
