036109ff25adc1d9fff2c0fdf2e499f3500ce3b1
wlt30 <wlt@ualberta.ca>
2020-04-03 19:13:45 -0600

    copying Jolene's changes

--- sd/views.py
+++ sd/views.py
@@ -40,7 +40,7 @@ def explore(request):
             comments = []
             for c in all_comments:
                 comments.append({
-                    'post':c.post.uuid,
+                    'post': str(c.post.uuid),
                     'author': c.author,
                     'comment': c.comment,
                     'published': c.published
