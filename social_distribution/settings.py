15ec7b21f3e9f06946d930fc40b1135ae16c51ac
nhervieu <nhervieu@ualberta.ca>
2020-04-05 20:28:48 -0600

    making image links smaller

--- social_distribution/settings.py
+++ social_distribution/settings.py
@@ -141,13 +141,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Canada/Mountain'
 
-DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S'
-
 USE_I18N = True
 
-USE_L10N = False
+USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
