4be9829031bb4ad08424e3eafe60257c4e37c18b
Warren Thomas <35236790+wlt30@users.noreply.github.com>
2020-04-08 13:18:44 -0600

    Wlt morefixes (#179)
    * working on markdown display
    
    * added img.post-content to css and added class to html for content
    
    * changed to default img for unspecified id
    
    * does this fix icons
    
    * moved img to bottom for ordering
    
    * embedding class in content
    
    * embedding class in content
    
    * content-image
    
    * content-image
    
    * going with NAtalies thing
    
    * using replace to inject add
    
    * same fix to feed
    
    * code cleanup
    
    * adding ajax.py
    
    * removed error from css
    
    * fixing deletepost
    
    * changed boxing on feed image to match content-image
    
    * content-image matching feedimage
    
    * changed /verify to /verifyuser

--- sd/static/sd/helperfunctions.js
+++ sd/static/sd/helperfunctions.js
@@ -33,20 +33,29 @@ function confirmDelete(post) {
     "Are you sure you want to delete this post?\nThis action cannot be undone."
   );
 
+  const data = {
+    target_post: post
+  };
+
   if (yes) {
-    fetch(origin + "/delete/" + post, {
+    fetch(origin + "/deletepost", {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json"
-      }
-    }).then(response => {
-      if (response.status === 403) {
-        console.log("Forbidden: Cannot delete posts of other users");
-      } else {
-        console.log("Post deleted");
-      }
-      location.reload();
-    });
+      },
+      body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      alert(
+        `Post deleted!`
+        );
+        location.reload();
+      })
+      .catch(function(error) {
+        alert(
+          "Couldn't delete post.\nPlease try again later."
+          );
+      });
   }
 }
 
@@ -81,7 +90,7 @@ function confirmDeleteUser(author) {
       })
       .catch(function(error) {
         alert(
-          "Couldn't verify user.\nPlease try again later."
+          "Couldn't delete user.\nPlease try again later."
           );
       });
     }
@@ -97,7 +106,7 @@ function confirmVerify(author) {
     const data = {
       target_author: author
     };
-    fetch(origin + "/verify", {
+    fetch(origin + "/verifyuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
--- sd/urls.py
+++ sd/urls.py
@@ -3,104 +3,56 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls import url
 from .views import *
 from .api_views import *
+from .ajax import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-
+     # API Calls
     url(r'^auth/register/$', CreateAuthorAPIView.as_view(), name='auth_user_create'),
     url(r'^auth/logout/$', LogoutView.as_view(), name='auth_user_logout'),
     url(r'^auth/getuser/$', GetAuthorAPIView.as_view(), name='auth_user_get'),
-    path('auth/edituser/<uuid:pk>',
-         EditUserView.as_view(), name='auth_user_update'),
+    path('auth/edituser/<uuid:pk>', EditUserView.as_view(), name='auth_user_update'),
     url(r'^auth/createpost/$', CreatePostAPIView.as_view(), name='auth_post_create'),
     url(r'^auth/getpost/$', GetPostAPIView.as_view(), name='auth_post_get'),
-    path('deletepost/<uuid:pk>',
-         DeletePostAPIView.as_view(), name='post_delete'),
-
-
-    # Get post (uuid of post)
+    path('deletepost/<uuid:pk>', DeletePostAPIView.as_view(), name='post_delete'),
     path('posts/<uuid:pk>', GetPostAPIView.as_view(), name='get_post'),
-
-    # Get post comments / Post post comment
-    path('posts/<uuid:pk>/comments',
-         CommentsAPIView.as_view(), name='get_post_comments'),
-
-    # Create post
+    path('posts/<uuid:pk>/comments', CommentsAPIView.as_view(), name='get_post_comments'),
     path('author/<uuid:pk>/post', CreatePostAPIView.as_view(), name='create_post'),
-
-    # Create friend request
-    path('friendrequest/', CreateFriendRequestAPIView.as_view(),
-         name='create_friend_request/'),
-
-
-    # Get all friend requests by author
-    path('author/<uuid:pk>/friendrequest',
-         GetAllAuthorFriendRequest.as_view(), name='all_author_friend_request'),
-
-    # Get all posts by author
-    path('author/<uuid:pk>/posts/', GetAllAuthorPostAPIView.as_view(),
-         name='all_author_posts'),
-
-    # Get all public posts
-    path('posts',
-         GetAllPublicPostsAPIView.as_view(), name='get_all_posts'),
-
-    # Get all posts visible to user
-    path('author/posts', GetAllVisiblePostsAPIView.as_view(),
-         name='get_visible_posts'),
-
-    # Get author object
+    path('friendrequest/', CreateFriendRequestAPIView.as_view(), name='create_friend_request/'),
+    path('author/<uuid:pk>/friendrequest', GetAllAuthorFriendRequest.as_view(), name='all_author_friend_request'),
+    path('author/<uuid:pk>/posts/', GetAllAuthorPostAPIView.as_view(), name='all_author_posts'),
+    path('posts', GetAllPublicPostsAPIView.as_view(), name='get_all_posts'),
+    path('author/posts', GetAllVisiblePostsAPIView.as_view(), name='get_visible_posts'),
     path('author/<uuid:pk>', GetAuthorAPIView.as_view(), name='get_author'),
-
-    # Get all author's friends
-    path('author/<uuid:pk>/friends',
-         GetAllAuthorFriendsAPIView.as_view(), name='all_author_friends'),
-
-    # Get all author's foaf (includes friends)
-    path('friend/<uuid:pk>/foaf',
-         GetAllFOAFAPIView.as_view(), name='get_author_foaf'),
-
-    # Get all authors
+    path('author/<uuid:pk>/friends', GetAllAuthorFriendsAPIView.as_view(), name='all_author_friends'),
+    path('friend/<uuid:pk>/foaf', GetAllFOAFAPIView.as_view(), name='get_author_foaf'),
     path('author/', GetAllAuthorsAPIView.as_view(), name='all_authors'),
-
-    # Create friend (pk of friend request)
     path('friend/<uuid:pk>', CreateFriendAPIView.as_view(), name='create_friend'),
-
-    # check if 2 authors are friends
-    path('author/<uuid:pk1>/friends/<uuid:pk2>',
-         CheckFriendAPIView.as_view(), name='check_friend'),
-
-    # Delete friend (pk of friend)
-    path('friend/<uuid:pk>/delete',
-         DeleteFriendAPIView.as_view(), name='delete_friend'),
+    path('author/<uuid:pk1>/friends/<uuid:pk2>', CheckFriendAPIView.as_view(), name='check_friend'),
+    path('friend/<uuid:pk>/delete', DeleteFriendAPIView.as_view(), name='delete_friend'),
 
 
-    # url(r'^author/<uuid:pk>/friends/<uuid:pk2>',
-    #     GetAllAuthorFriends.as_view(), name='get_all_author_friends'),
-    # url(r'^author/<uuid:pk>/friends/<uuid:pk>',
-    #     GetAllAuthorFriends.as_view(), name='get_all_author_friends'),s
-
+     #Django views
     path('', explore, name='explore'),
     path('login', login, name='login'),
     path('logout/', logout, name='logout'),
     path('register/', register, name='register'),
-
     path('newpost', new_post, name='new_post'),
     path('edit/<uuid:pk>', edit_post, name='edit_post'),
     path('feed', feed, name="my_feed"),
-
     path('notifications', notifications, name='notifications'),
-    path('friendrequest', friendrequest, name='friend_request'),
-     path('rejectrequest', rejectrequest, name='reject_request'),
-    path('delete/<uuid:post_id>', delete_post, name='delete_post'),
     path('edit_post/<uuid:post_id>', edit_post, name='edit_post'),
     path('image/<uuid:pk>', get_image, name='get_image'),
-
-    # """Optional Pages"""
     path('search', search, name='search'),
     path('account', account, name='account'),
     path('edit_account', edit_account, name='edit_account'),
-    path('unfollow', unfollow, name='unfollow'),
     path('verify', verify, name='verify'),
+
+    #AJAX Calls
+    path('verifyuser', verifyuser, name='verifyuser'),
     path('deleteuser', deleteuser, name='deleteuser'),
+    path('rejectrequest', rejectrequest, name='reject_request'),
+    path('friendrequest', friendrequest, name='friend_request'),
+    path('unfollow', unfollow, name='unfollow'),
+    path('deletepost', deletepost, name='delete_post'),
 ]
